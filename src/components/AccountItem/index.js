02aaf7dccf014ddb7bac2b5d26fd04e48f6d7680
Phat <nguyenthanhphat13032005@gmail.com>
2025-03-23 22:10:05 +0700

    UI header #3

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,26 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
+
 function AccountItem() {
-  return (
-    <div className={cx('wrapper')}>
-      <img
-        className={cx('avatar')}
-        src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/1f19e66f3be22e2a94363b66639073c7~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=21776db9&x-expires=1742472000&x-signature=GaznE96oGxOrsgOokCJVYwPCcFw%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my"
-        alt="Hoa"
-      />
-      <div className={cx('info')}>
-        <h4 className={cx('name')}>
-          <span>Nguyen van a</span>
-          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-        </h4>
-        <span className={cx('username')}>usename</span>
-      </div>
-    </div>
-  );
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/ea0854578085ab26effc2c7b8cefa270~c5_300x300.webp?x-expires=1651489200&x-signature=Xp8iH14MDegegBpFEg%2Fg5rv6Mrw%3D"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
 }
 
 export default AccountItem;
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,62 +1,67 @@
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
-const cx = classNames.bind(styles);
-function Button({
-  to,
-  href,
 
-  outline = false,
-  primary = false,
-  small = false,
-  large = false,
-  text = false,
-  disabled = false,
-  rounded = false,
-  className,
-  leftIcon,
-  rightIcon,
-  children,
-  onClick,
-  ...passProps
-}) {
-  let Comp = 'button';
-  const props = {
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    rounded = false,
+    disabled = false,
+    small = false,
+    large = false,
+    children,
+    className,
+    leftIcon,
+    rightIcon,
     onClick,
-    ...passProps,
-  };
-  //  remove event listener when btn is disabled
-  if (disabled) {
-    Object.keys(props).forEach((key) => {
-      if (key.startsWith('on') && typeof props[key] === 'function') {
-        delete props[key];
-      }
+    ...passProps
+}) {
+    let Comp = 'button';
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    // Remove event listener when btn is disabled
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline,
+        text,
+        disabled,
+        rounded,
+        small,
+        large,
     });
-  }
-  if (to) {
-    props.to = to;
-    Comp = Link;
-  } else if (href) {
-    props.href = href;
-    Comp = 'a';
-  }
-  const classes = cx('wrapper', {
-    [className]: className,
-    primary,
-    outline,
-    small,
-    large,
-    text,
-    disabled,
-    rounded,
-  });
-  return (
-    <Comp className={classes} {...props}>
-      {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
-      <span className={cx('title')}>{children}</span>
-      {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
-    </Comp>
-  );
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
 }
 
 export default Button;
